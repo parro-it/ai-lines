70b8ecab55df54f20be2b1805824534fe56a960c
Andrea Parodi <andrea@parro.it>
2017-11-18 12:07:12 +0100

    first version

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-export default function aiLines() {
-  return 42;
+export default async function* aiLines(stream, separator = /\n/) {
+  let buff = { concat: s => s };
+  for await (const chunk of stream) {
+    const lines = chunk.split(separator);
+    const firstLine = lines.shift();
+    const lastLine = lines.pop();
+
+    buff = buff.concat(firstLine);
+
+    if (typeof lastLine === "string") {
+      yield buff;
+      buff = lastLine;
+
+      for (const otherPart of lines) {
+        yield otherPart;
+      }
+    }
+  }
+
+  if (typeof buff === "string") {
+    yield buff;
+  }
 }
--- test.js
+++ test.js
@@ -1,6 +1,56 @@
 import test from "tape-async";
-import aiLines from ".";
+import lines from ".";
+import concat from "ai-concat";
+import AsyncIterable from "asynciterable";
 
-test("exports a function", t => {
-  t.is(typeof aiLines, "function");
+function fromArray(arr) {
+  return new AsyncIterable((write, end) => {
+    arr.forEach(write);
+    end();
+  });
+}
+
+test("exports a function", async t => {
+  t.is(typeof lines, "function");
+});
+
+test("emit each chunk as separated line", async t => {
+  const textLines = ["test line 1", "line 2"];
+  const inLines = lines(fromArray([textLines.join("\n")]));
+  t.deepEqual(await concat.obj(inLines), textLines);
+});
+
+test("3 lines", async t => {
+  const textLines = ["line 1", "line 2", "line 3"];
+  const inLines = lines(fromArray([textLines.join("\n")]));
+  t.deepEqual(await concat.obj(inLines), textLines);
+});
+
+test("empty lines", async t => {
+  const textLines = ["line 1", "", "line 3"];
+  const inLines = lines(fromArray([textLines.join("\n")]));
+  t.deepEqual(await concat.obj(inLines), textLines);
+});
+
+test("empty lines at start and end", async t => {
+  const textLines = ["", "line 1", "", "line 3", ""];
+  const inLines = lines(fromArray([textLines.join("\n")]));
+  t.deepEqual(await concat.obj(inLines), textLines);
+});
+
+test("single line", async t => {
+  const textLines = ["line 3"];
+  const inLines = lines(fromArray([textLines.join("\n")]));
+  t.deepEqual(await concat.obj(inLines), textLines);
+});
+
+test("single empty line", async t => {
+  const textLines = [""];
+  const inLines = lines(fromArray([textLines.join("\n")]));
+  t.deepEqual(await concat.obj(inLines), textLines);
+});
+
+test("empty stream", async t => {
+  const inLines = lines(fromArray([]));
+  t.deepEqual(await concat.obj(inLines), []);
 });
